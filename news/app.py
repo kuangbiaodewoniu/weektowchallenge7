e179dc3d8a1160a28e2e7c28e7abf348b1170c1b
dandan.zheng <cha.baby@qq.com>
2018-03-22 18:07:04 +0800

    优化文件

--- news/app.py
+++ news/app.py
@@ -6,36 +6,10 @@
 @time: 2018/03/21 
 """
 from flask import Flask, render_template
-import json,os
+import os
 from models.db import get_file_data, get_data_byid
 
 app = Flask(__name__)
-pwd = os.getcwd()
-news_dir = os.path.abspath(os.path.dirname(pwd)+os.path.sep+"files")
-
-
-# 从文件中获取json数据
-def get_data(json_file):
-    try:
-        with open(json_file) as re:
-            data = json.load(re)
-            return data
-    except:
-        return []
-
-
-# 取标题
-def get_title(data):
-    return data.get('title')
-
-
-# 获取文件
-def get_files(dir):
-    files = []
-    for file_name in os.listdir(dir):
-        file = os.path.join(dir, file_name)
-        files.append(file)
-    return files
 
 
 @app.route('/')
